289a2e0949d68bd6aac9c04d6d709bb469febf48
Ritesh Patel <patelritesh261@gmail.com>
2016-08-06 10:29:23 -0400

    Add project files.

---
+++ LAB5/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace LAB5.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LAB5.Models.MusicStoreContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "LAB5.Models.MusicStoreContext";
+        }
+
+        protected override void Seed(LAB5.Models.MusicStoreContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
